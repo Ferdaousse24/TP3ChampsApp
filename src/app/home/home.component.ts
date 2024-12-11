5b34d9938a280a82194e5d9d08fffd2164134502
Ferdaouse24 <elbaraka23ferdaousse@gmail.com>
2024-12-11 11:09:21 +0100

    td03 finie

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // Importer FormsModule
 import { CommonModule } from '@angular/common'; // Import du CommonModule
+import { animate, keyframes, query, stagger, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [FormsModule,CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+
+  animations : [ trigger('animobjectifs' ,[
+    transition ( '*=>*' ,[
+    query(':enter', style({opacity: 0}), {optional: true}),
+    query(':enter', stagger('300ms',[ animate ( '.6s ease-in' , keyframes ([
+    style({opacity: 0, transform: 'translateY(-75%)', offset: 0}), style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
+    style({opacity: 1, transform: 'translateY(0)', offset: 1}), ]))]),{optional: true})
+    ]) ])]
+
+
 })
 export class HomeComponent {
   nbItems = 0 ; 
